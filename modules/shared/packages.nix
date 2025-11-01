d2fe019be4f21a5ebeaccad8bf0ad7f937160752
ChrisLAS <chris@jupiterbroadcasting.com>
2025-11-01 14:12:39 -0700

    packages: resolve rofi Wayland package across nixpkgs versions (rofi-wayland/rofi-wayland-unwrapped/rofi)

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -2,18 +2,22 @@
 let
   cfg = config.hyprvibe.packages;
   # Curated common sets derived from overlaps across hosts
+  rofiPkg = if pkgs ? rofi-wayland then pkgs.rofi-wayland
+            else if pkgs ? rofi-wayland-unwrapped then pkgs.rofi-wayland-unwrapped
+            else pkgs.rofi;
   basePackages = with pkgs; [
     htop btop tree lsof lshw neofetch nmap zip unzip gnupg curl file jq bat fd fzf ripgrep tldr
     whois plocate less eza grc
   ];
-  desktopPackages = with pkgs; [
-    wl-clipboard grim slurp swappy wf-recorder dunst cliphist brightnessctl playerctl pavucontrol
-    qt6Packages.qt6ct
-    # Core desktop apps
-    kitty rofi-wayland
-    # Hyprland companions started by base config
-    hyprpaper hypridle hyprlock
-  ];
+  desktopPackages =
+    (with pkgs; [
+      wl-clipboard grim slurp swappy wf-recorder dunst cliphist brightnessctl playerctl pavucontrol
+      qt6Packages.qt6ct
+      # Core desktop apps
+      kitty
+      # Hyprland companions started by base config
+      hyprpaper hypridle hyprlock
+    ]) ++ [ rofiPkg ];
   devPackages = with pkgs; [
     git gh gitui gcc gnumake cmake binutils patchelf python3 go nodejs_20 yarn imagemagick
   ];
